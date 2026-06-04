640fa408264e0b68437d24683562635f9edd5b9a
donishadsmith <donishasmith@outlook.com>
2026-06-03 23:14:08 -0400

    Update

---
+++ Rust/definitions/definitions.rs
@@ -0,0 +1,24 @@
+
+use std::{collections::HashMap, ops::Range};
+
+enum Item {
+    CrystalBall,
+}
+
+struct Player {
+    hp: i8,
+    mana: i8,
+    money: i32,
+    attack_power: Range<i8>,
+    current_position: (i8, i8, i8),
+    inventory: HashMap<i8, Item>,
+}
+
+struct Zombie {
+    halt: bool,
+    current_position: (i8, i8, i8),
+    distance_from_player: u32,
+}
+
+fn main() {
+}
